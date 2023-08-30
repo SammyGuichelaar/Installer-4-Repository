d9137107ae6f91f62854beaf2795e94998072d92
Sammy Guichelaar <30523291+SammyGuichelaar@users.noreply.github.com>
2023-08-30 13:00:41 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Installer 4 Repository Script
-PHP based repository script for the Installer 4 package manager
+# Installer BETA Repository Code v0.1.2b
+
+Fixed in v0.1.2b: 
+- Added sanity checks to avoid XSS type attacks.
+- Fixed an issue where macOS GUI zipped packages wouldn’t be processed.
+
+Installer PRELIMINARY repository code to set up your own Installer repository.
+
+Please note that this code is in the early stages of beta. It can change a lot for the public release of Installer.
+
+Instructions for hosting your own repository in 2 minutes:
+
+1. Upload all files via FTP
+2. In the folder packages, you can create folders, which will be treated by Installer as categories. 
+  If one of your folders is named 'SpringBoard Tweaks' installer will create a category called SpringBoard Tweaks.
+  Add zip packages to the category folders.
+3. Make sure 'info' folder is writeable by the http user. (Change permissions) 
+4. Open config.inc.default.php
+5. Find (line 33) define('REGENERATE_SECRET', '');
+  Create your secret key. define('REGENERATE_SECRET', 'SecretKey12345');
+6. Find (line 40) $POSSIBLE_FIRMWARE_VERSIONS = array( '8.0', '8.0.1', '10.0', '11.1.1' );
+  Add the firmwares you want your repository to support
+  Close config.inc.default.php and rename to config.inc.php
+7. Regenerate your repository by calling http://mywebsite.com/regenerate.php?secret=secretkeyhere
+
+All done. The Installer repository is now ready. When the beta becomes available, please test your repo. 
---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+/*
+	Installer 5 Repository
+	© 2017-2018, Infini Dev, © 2008 Slava Karpenko
+
+	Should you have any questions, do not hesitate to send them at @AppTappSoftware.
+*/
+
+if (!file_exists("config.inc.php"))
+	die("config.inc.php does not exist.");
+
+require_once("config.inc.php");
+
+// No user serviceable parts beyond this point.
+
+function IndexFilename() {
+	global $os_version;
+	return (INFO_PATH . "/index-" . $os_version . ".plist");
+}
+
+ob_start("ob_gzhandler");
+
+$installer_version = @sanitizeVariable($_REQUEST['installerVersion']);
+$os_version = @sanitizeVariable($_REQUEST['firmwareVersion']);
+$platform = @sanitizeVariable($_REQUEST['platform']);
+$deviceUUID = @sanitizeVariable($_REQUEST['deviceUUID']);
+
+if (!$os_version || !in_array($os_version, $POSSIBLE_FIRMWARE_VERSIONS))
+	$os_version = DEFAULT_FIRMWARE;
+
+if(!@sanitizeVariable($_GET['debug']) && !(strstr($_SERVER['HTTP_USER_AGENT'], 'Install') || strstr($_SERVER['HTTP_USER_AGENT'], 'CFNetwork'))) {
+	include("instructions.php");
+	exit;
+}
+
+$debug = @sanitizeVariable($_GET['debug']) && ENABLE_DEBUG;
+
+if ($debug)
+	header('Content-Type: text/plain; charset=utf-8');
+else
+	header('Content-Type: application/x-install-repository; charset=utf-8');
+
+$index_file = IndexFilename();
+
+if (!file_exists($index_file))				// if there's no pre-built index cached, return a blank file.
+	print file_get_contents("Info.plist");
+else
+	print file_get_contents($index_file);
+
+ob_end_flush();
+
+exit;
+
+?>
